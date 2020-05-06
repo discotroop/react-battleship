0a28df4da5ba695bce16f60cc63337c119466566
John Mark Goodson <john.mark.hat@gmail.com>
2020-05-06 14:01:50 -0400

    Updates

---
+++ precache-manifest.0a04d6c8f00b9e50bd8e23aeaef29e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4623a62f57fdb0c35fa4d5916f5826",
+    "url": "/react-battleship/index.html"
+  },
+  {
+    "revision": "6fb2fd8ebb135c382010",
+    "url": "/react-battleship/static/css/main.6986df8d.chunk.css"
+  },
+  {
+    "revision": "4b292435fb33b460cbbf",
+    "url": "/react-battleship/static/js/2.5529ff42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-battleship/static/js/2.5529ff42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fb2fd8ebb135c382010",
+    "url": "/react-battleship/static/js/main.f9a18db6.chunk.js"
+  },
+  {
+    "revision": "e7deea1e36a4eb10920a",
+    "url": "/react-battleship/static/js/runtime-main.b7ecefab.js"
+  }
+]);
